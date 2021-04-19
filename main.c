27323ae2da1cf8aa009e5b574cf7910a7107718d
suhas-kamath <82808712+suhas-kamath@users.noreply.github.com>
2021-04-19 15:15:11 +0530

    added main.c file

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{    while(1){
+     float a,b,res;
+     int x=0;
+     char oper;
+
+
+    printf("enter the number a:" ) ;
+     scanf("%f",&a);
+    printf ("enter the number b:");
+    scanf("%f",&b);
+     printf("enter the operator:");
+     scanf(" %c",&oper);
+     switch(oper)
+     {
+         case '+' :
+            res=a+b;
+            break;
+
+         case '-' :
+            res=a-b;
+            break;
+
+         case '/' :
+             if(b==0)
+            {
+            printf("error");
+            x=1;
+            }
+             else
+                res=a/b;
+                break;
+
+         case '*' :
+             res=a*b;
+             break;
+
+        default:
+            printf("invalid operator");
+            x=1;
+     }
+   if (x=0){
+     printf ( "the result is:%f\n",res);
+   }
+}
+}
+
