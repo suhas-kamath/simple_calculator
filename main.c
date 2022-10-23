3512d1d85b9dd04fbb52ff13f362011834415934
Paramashiva Karanth <86004162+shiva-karanth@users.noreply.github.com>
2022-10-23 12:10:56 +0530

    Update main.c
    updated logic in the code

--- main.c
+++ main.c
@@ -1,50 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main()
-{    while(1){
-     float a,b,res;
-     int x=0;
-     char oper;
+void
+main ()
+{
+  while (1)
+    {
+      float a, b, res;
+
+      char oper;
 
 
-    printf("enter the number a:" ) ;
-     scanf("%f",&a);
-    printf ("enter the number b:");
-    scanf("%f",&b);
-     printf("enter the operator:");
-     scanf(" %c",&oper);
-     switch(oper)
-     {
-         case '+' :
-            res=a+b;
-            break;
+      printf ("enter the number a:");
+      scanf ("%f", &a);
+      printf ("enter the number b:");
+      scanf ("%f", &b);
+      printf ("enter the operator:");
+      scanf (" %c", &oper);
+      switch (oper)
+	{
+	case '+':
+	  res = a + b;
+	  break;
 
-         case '-' :
-            res=a-b;
-            break;
+	case '-':
+	  res = a - b;
+	  break;
 
-         case '/' :
-             if(b==0)
-            {
-            printf("error");
-            x=1;
-            }
-             else
-                res=a/b;
-                break;
+	case '/':
+	  if (b == 0)
+	    {
+	      printf ("error");
 
-         case '*' :
-             res=a*b;
-             break;
+	    }
+	  else
+	    res = a / b;
+	  break;
 
-        default:
-            printf("invalid operator");
-            x=1;
-     }
-   if (x=0){
-     printf ( "the result is:%f\n",res);
-   }
+	case '*':
+	  res = a * b;
+	  break;
+	case '%':
+	  if (b == 0)
+	    {
+	      printf ("error");
+
+	    }
+	  else
+	    res = (int) a % (int) b;
+	  break;
+	default:
+	  printf ("invalid operator");
+
+	}
+      printf ("the result is:%f\n", res);
+
+    }
 }
-}
-
